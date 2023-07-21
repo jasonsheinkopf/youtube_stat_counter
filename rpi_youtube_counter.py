987381e65ae3eac0aa0b13c9beab1035a53dc10e
Ghost Popsicle <jasonsheinkopf@gmail.com>
2023-07-21 19:12:05 +0900

    Code ready to go on rpi

--- rpi_youtube_counter.py
+++ rpi_youtube_counter.py
@@ -3,15 +3,26 @@ import googleapiclient.errors
 import pygame
 import os
 from video_stats import Video
+from moviepy.editor import VideoFileClip
+import pyglet
+import random
+import datetime
+import pytz
+import colorsys
+
 
 # Your personal API key and channel ID
 API_KEY = 'AIzaSyDzq_TFOT8qNbSHrbMz1Y_F5mpKrUXOq3s'
 CHANNEL_ID = 'UCPAtrpdvJTHjy0c19zlWBGw'
 
+# List to store video objects
+video_list = []
+api_calls = 0
+
 def get_video_view_counts(channel_id, api_key):
-    'Creates a list of video objects from youtube channel'
+    'Fetches video details and view counts for videos in a channel'
     youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
-    video_list = []
+    global video_list, api_calls
 
     try:
         # Get the list of uploaded videos for the channel
@@ -19,6 +30,7 @@ def get_video_view_counts(channel_id, api_key):
             part='contentDetails',
             id=channel_id
         ).execute()
+        api_calls += 1
 
         # Extract the uploads playlist ID
         uploads_playlist_id = playlist_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
@@ -29,44 +41,54 @@ def get_video_view_counts(channel_id, api_key):
             playlistId=uploads_playlist_id,
             maxResults=50
         ).execute()
+        api_calls += 1
 
-        # Iterate over the videos and fetch their view counts
-        for item in videos_response['items']:
-            video_id = item['snippet']['resourceId']['videoId']
+        # Prepare a list of video IDs
+        video_ids = [item['snippet']['resourceId']['videoId'] for item in videos_response['items']]
+
+        # Fetch video statistics using batch request
+        video_statistics_response = youtube.videos().list(
+            part='snippet,statistics',
+            id=','.join(video_ids)
+        ).execute()
+        api_calls += 1
+
+        # Flag to keep track of whether video object already created
+        video_exists = False
+        
+        # Combine video details and view counts into video_data list
+        for item in video_statistics_response['items']:
+            video_id = item['id']
             video_title = item['snippet']['title']
-            video_response = youtube.videos().list(
-                part='statistics',
-                id=video_id
-            ).execute()
-
-            # Extract the view count from the response
-            view_count = int(video_response['items'][0]['statistics']['viewCount'])
-            # video_list.append({video_title: view_count})
-
-            # Flag to keep track of whether video has already been created
-            video_exists = False
+            view_count = int(item['statistics']['viewCount'])
+            
             # Iterate through list of video objects
             for video in video_list:
-                # If the video object already exists
+                # Check if video object has already been added to list
                 if video.title == video_title:
-                    # Count how many views added
+                    # Record views added
                     video.views_added = video.current_views - video.prev_views
-                    # Set previous views to current for next api call
+                    # Set prev views to current for next cycle
                     video.prev_views = video.current_views
-                    # Set flag to True to indicated video was found
-                    video_found = True
+                    # Set flag to True indicating video was found
+                    video_exists = True
                     break
-            # If the video_title is not found anywhere in the list
+
+            # If video object is not yet in list
             if not video_exists:
                 # Create new video object
                 new_video = Video(title=video_title, current_views=view_count)
                 # Append it to the list
                 video_list.append(new_video)
-
+                print(f"Video {video_title} added")
+            
+        # for video in video_list:
+        #     print(video.title)
+        print(f"Api calls: {api_calls}")
         return video_list
-
+    
     except googleapiclient.errors.HttpError as e:
-        print(f"An error occurred: {e}")
+        print(f"an error occurred: {e}")
 
 def get_subscribers_count(channel_id, api_key):
     youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
@@ -86,9 +108,39 @@ def get_subscribers_count(channel_id, api_key):
     except googleapiclient.errors.HttpError as e:
         print(f"An error occurred: {e}")
 
-# Get initial subscribers count
-subscriber_count = get_subscribers_count(CHANNEL_ID, API_KEY)
-prev_subscriber_count = subscriber_count
+def wake_sleep():
+    # Get current time
+    current_time = datetime.datetime.now(timezone)
+
+    # Turn off sounds during sleep hours
+    if wake_hour < current_time.hour <= sleep_hour:
+        pygame.mixer.unpause()
+    else:
+        pygame.mixer.pause()
+
+def get_rainbow_color():
+    # Get the elapsed time in milliseconds
+    elapsed_time = pygame.time.get_ticks()
+    
+    # Adjust the hue value (range 0.0 to 1.0) based on elapsed time
+    hue = (elapsed_time % 1000) / 1000.0  # Change 10000 to control the speed of the color cycling
+
+    # Convert HSV to RGB
+    rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+    
+    # Scale RGB values from (0.0 to 1.0) to (0 to 255)
+    red = int(rgb[0] * 255)
+    green = int(rgb[1] * 255)
+    blue = int(rgb[2] * 255)
+    
+    # Return the color as a tuple
+    return red, green, blue
+
+# Set timezone
+timezone = pytz.timezone('Asia/Tokyo')
+# Time when awake hours begins
+wake_hour = 9
+sleep_hour = 21
 
 # Set starting postion of pygame window
 screen_x = 0
@@ -102,6 +154,37 @@ pygame.init()
 width, height = 320, 480  # Set the window size according to your touch screen
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Akiya Quest Stats")
+
+# View audio dir
+view_aud_dir = 'media/view_sounds'
+
+# Initialize an empty list to store paths
+view_aud_list = []
+
+# Iterate through all files and directories in the folder
+for root, dirs, files in os.walk(view_aud_dir):
+    for file in files:
+        # Get the absolute path of each file
+        file_path = os.path.join(root, file)
+        view_aud_list.append(file_path)
+
+# Create sub audio and video object
+sub_vid_path = "./media/pon_chorus.mp4"
+sub_aud_path = "./media/pon_chorus.mp3"
+sub_vid = VideoFileClip(sub_vid_path)
+
+# Set video location and scale factor
+video_x, video_y = 180, 85
+sub_vid_sf = 0.13
+
+# Resize video
+aspect_ratio = sub_vid.w / sub_vid.h
+new_sub_vid_w = int(sub_vid.w * sub_vid_sf)
+new_sub_vid_h = int(sub_vid.h * sub_vid_sf)
+sub_vid = sub_vid.resize((new_sub_vid_w, new_sub_vid_h))
+
+# Set bool to false
+video_playing = False
 
 # Load logo
 logo_sf = 0.07
@@ -132,15 +215,28 @@ font_color = (0, 0, 0)
 font = pygame.font.Font(font_path, font_size)
 
 # How many seconds between stat checks
-wait_time = 60
+wait_time = 5
 
 # Create a clock object
 clock = pygame.time.Clock()
 time_since_last_check = 0
 
+# Initialize subscriber count
+subscriber_count = get_subscribers_count(CHANNEL_ID, API_KEY)
+prev_subscriber_count = subscriber_count
+
+# Initialize view counts
+video_view_counts = get_video_view_counts(CHANNEL_ID, API_KEY)
+
 # Run the game loop
 running = True
 while running:
+    # Cycle through colors based on ticks
+    rainbow_color = get_rainbow_color()
+    
+    # Handle wake sleep
+    wake_sleep()
+
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -149,12 +245,11 @@ while running:
     # Check time elapsed since last stat update
     time_since_last_check += clock.tick()
 
-    # Get initial view and subscriber counts
-    video_view_counts = get_video_view_counts(CHANNEL_ID, API_KEY)
-    subscriber_count = get_subscribers_count(CHANNEL_ID, API_KEY)
+    # Initialize total_new_views
+    total_new_views = 0
 
     # Check if it's time to check new stats
-    if time_since_last_check >= wait_time * 1000:
+    if time_since_last_check >= wait_time * 1000 and video_playing == False:
         time_since_last_check = 0
 
         # Get video view counts
@@ -162,11 +257,21 @@ while running:
 
         # Get subscribers count
         subscriber_count = get_subscribers_count(CHANNEL_ID, API_KEY)
-        if subscriber_count > prev_subscriber_count:
-            print("Play subscriber animation")
 
         # Count new views for each video
         [video.count_new_views() for video in video_view_counts]
+
+        # Count total new views
+        for video in video_view_counts:
+            total_new_views += video.views_added
+      
+        # Check if there are any new views
+        if total_new_views > 0:
+            # Randomly select a sound effect from the dir
+            view_sound_object = pygame.mixer.Sound(random.choice(view_aud_list))
+            duration = int(view_sound_object.get_length() * 1000)
+            view_sound_object.set_volume(1)
+            view_sound_object.play(maxtime=duration)
 
     # Clear the window
     window.fill((255, 255, 255))  # Set the background color to white
@@ -176,19 +281,20 @@ while running:
     window.blit(resized_title, (125, 25))
     
     # Render the subscriber text
-    text_subscribers = font.render(f"Subscribers  {subscriber_count}", True, font_color)  # Set the text color to black
+    subscriber_font = rainbow_color if subscriber_count - prev_subscriber_count > 0 else font_color
+    text_subscribers = font.render(f"{subscriber_count} subscribers", True, subscriber_font)  # Set the text color to black
     window.blit(text_subscribers, (160, 60))  # Adjust the position of the text according to your layout
 
     # Enable underline for the font
     font.set_underline(True)
 
     # Render the views heading
-    views_heading = font.render(f"VIEWS", True, font_color)  # Set the text color to black
-    window.blit(views_heading, (40, 120))  # Adjust the position of the text according to your layout
+    views_heading = font.render(f"Views", True, font_color)  # Set the text color to black
+    window.blit(views_heading, (60, 125))  # Adjust the position of the text according to your layout
 
     # Render the title heading
-    title_heading = font.render(f"TITLE", True, font_color)  # Set the text color to black
-    window.blit(title_heading, (125, 120))  # Adjust the position of the text according to your layout
+    title_heading = font.render(f"Title", True, font_color)  # Set the text color to black
+    window.blit(title_heading, (125, 125))  # Adjust the position of the text according to your layout
 
     # Enable underline for the font
     font.set_underline(False)
@@ -198,20 +304,58 @@ while running:
     
     # Render video text
     for i, video in enumerate(video_view_counts):
-        video_title = video.title
+        video_title = video.title.title()
         # Remove unnecessary prefix
         if "#" in video_title:
             video_title = video_title.split("#")[0]
         if "-" in video_title:
             video_title = video_title.split("-")[0]
-        view_count = video.current_views
-        count_text = font.render(str(view_count), True, font_color)
-        window.blit(count_text, (40, 150 + 20 * i))
-        video_text = font.render(video_title, True, font_color)
-        window.blit(video_text, (125, 150 + 20 * i))
+        # Print video count to screen
+        count_text = font.render(str(video.current_views), True, font_color)
+        window.blit(count_text, (60, 150 + 20 * i))
+        # Print title to screen
+        title_text = font.render(video_title, True, font_color)
+        window.blit(title_text, (125, 150 + 20 * i))
+        # Print new views added to screeen
+        if video.views_added > 0:
+            added_text = font.render("+" + str(video.views_added), True, rainbow_color)
+            window.blit(added_text, (25, 150 + 20 * i))
+
+    # Play video if new subscribers
+    if subscriber_count > prev_subscriber_count and not video_playing:   # change to >
+        sub_aud_object = pygame.mixer.Sound(sub_aud_path)
+        sub_aud_dur = int(sub_aud_object.get_length() * 1000)
+        sub_aud_object.set_volume(0.2)
+        sub_aud_object.play(maxtime=sub_aud_dur)
+        video_playing = True
+        video_start_time = pygame.time.get_ticks() / 1000
+    # Handle case if subscribers are lost
+    elif subscriber_count < prev_subscriber_count:
+        prev_subscriber_count = subscriber_count
+
+    # Check if the video is still playing
+    if video_playing:
+        elapsed_time = pygame.time.get_ticks() / 1000 - video_start_time
+        # Handle tick count error
+        sub_vid_frame_count = int(sub_vid.fps * sub_vid.duration)
+        if elapsed_time > sub_vid_frame_count:
+            elapsed_time = 0
+        frame = sub_vid.get_frame(elapsed_time)
+        pygame_frame = pygame.image.fromstring(frame.tobytes(), sub_vid.size, 'RGB')
+        window.blit(pygame_frame, (video_x, video_y))
+
+        # Check if the video has finished playing
+        if elapsed_time >= sub_vid.duration:
+            video_playing = False
+            # pygame.mixer.music.stop()
+            sub_vid.close()
+            prev_subscriber_count = subscriber_count
+            
 
     # Update the display
     pygame.display.flip()
+
+    # print(f"Sub: {subscriber_count} | PrevSub: {prev_subscriber_count}")
 
 # Quit Pygame
 pygame.quit()
